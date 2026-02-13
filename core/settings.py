2142329656059bd0fb96402e4f0ab33e01aafe24
Raúl Sánchez <rauulssanchezz@gmail.com>
2026-02-13 13:01:57 +0100

    urls y settings

--- core/settings.py
+++ core/settings.py
@@ -38,13 +38,16 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'rest_framework.authtoken',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-    'users'
+    'users',
+    'vets_users',
+    'clinics'
 ]
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from users.views import RegisterView
+from clinics.views import CreateClinicView
 from rest_framework.authtoken import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/register/', RegisterView.as_view(), name='register'),
-    path('api/login/', auth_views.obtain_auth_token, name='login')
+    path('api/auth/register/', RegisterView.as_view(), name='register'),
+    path('api/auth/login/', auth_views.obtain_auth_token, name='login'),
+    path('api/clinic/create', CreateClinicView.as_view(), name='create_clinic')
 ]
