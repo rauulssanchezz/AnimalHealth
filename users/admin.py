0ef60ab8b99f87f4dddfd094d2730a21a041665f
Raúl Sánchez <rauulssanchezz@gmail.com>
2026-02-13 13:00:30 +0100

    añadir users al panel de admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class MyUserAdmin(UserAdmin):
+    # Esto añade el ID (UUID) y el email al listado
+    list_display = ('email', 'username', 'is_staff', 'is_superuser')
+    
+    # Como tu login es por email, asegúrate de que el orden sea correcto
+    ordering = ('email',)
+    
+    # Esto es necesario si añadiste campos extra a tu modelo User
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('id',)}), # Por ejemplo para ver el UUID
+    )
+    readonly_fields = ('id',)
