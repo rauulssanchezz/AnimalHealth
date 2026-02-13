2b2ca033bd5ac4be81022975fedc6ba734d03327
Raúl Sánchez <rauulssanchezz@gmail.com>
2026-02-13 13:01:47 +0100

    user model

--- users/models.py
+++ users/models.py
@@ -1,11 +1,15 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=150, unique=False, blank=True, null=True)
 
     USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self) :
         return self.email
