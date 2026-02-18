c83032118270298bea69908bcc470e8878aca24d
Raúl Sánchez <rauulssanchezz@gmail.com>
2026-02-18 11:04:49 +0100

    mejorar users

--- users/serializers.py
+++ users/serializers.py
@@ -7,7 +7,6 @@ EMAIL = 'email'
 USERNAME = 'username'
 PASSWORD = 'password'
 IS_VET = 'is_vet'
-CLINIC = 'clinic'
 CLINIC_ADMIN = 'clinic_admin'
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -15,14 +14,20 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = [EMAIL, USERNAME, PASSWORD, IS_VET, CLINIC, CLINIC_ADMIN]
+        fields = [EMAIL, USERNAME, PASSWORD, IS_VET, CLINIC_ADMIN]
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'email': {'required': True}
+        }
 
     def create(self, validated_data):
-        return User.objects.create_user(
-            email=validated_data[EMAIL],
-            username=validated_data[USERNAME],
-            password=validated_data[PASSWORD],
-            IS_VET=validated_data[IS_VET],
-            CLINIC=validated_data[CLINIC],
-            CLINIC_ADMIN=validated_data[CLINIC_ADMIN]
-        )
+        password = validated_data.pop(PASSWORD)
+        
+        return User.objects.create_user(password=password, **validated_data)
+    
+    def update(self, instance, validated_data):
+        password = validated_data.pop(PASSWORD, None)
+        if password:
+            instance.set_password(password)
+        
+        return super().update(instance, validated_data)
--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status, permissions
+from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import RegisterSerializer
@@ -14,3 +14,25 @@ class RegisterView(APIView):
             return Response({"message": "Usuario creado con éxito"}, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+    
+    def delete(self, request):
+        user = request.user
+        user.delete()
+        return Response(
+            {"message": "Usuario eliminado correctamente"}, 
+            status=status.HTTP_204_NO_CONTENT
+        )
+    
+class LogoutView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response({"detail": "Sesión cerrada correctamente."}, status=status.HTTP_200_OK)
