8236a8499f071b0cd9a7a5f9f133330f54cc47c8
Raúl Sánchez <rauulssanchezz@gmail.com>
2026-02-18 12:11:51 +0100

    actualizar urls

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
-from users.views import LogoutView, RegisterView, UserProfileView
+from users.views import LogoutView, RegisterView, UserInformationView, UserProfileView, VetViewSet
 from clinics.views import ClinicViewSet
 from rest_framework.authtoken import views as auth_views
 from rest_framework.routers import DefaultRouter
@@ -10,12 +10,14 @@ router = DefaultRouter()
 
 router.register(r'clinics', ClinicViewSet, basename='clinic')
 router.register(r'rates', RateViewSet, basename='rate')
+router.register(r'vets', VetViewSet, basename='vets')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/register/', RegisterView.as_view(), name='register'),
     path('api/auth/login/', auth_views.obtain_auth_token, name='login'),
-    path('api/auth/me', UserProfileView.as_view(), name='user-profile'),
-    path('api/auth/logout', LogoutView.as_view(), name='logout'),
+    path('api/users/me/', UserProfileView.as_view(), name='user-profile'),
+    path('api/auth/logout/', LogoutView.as_view(), name='logout'),
+    path('api/users/<uuid:pk>/', UserInformationView.as_view(), name='user-information'),
     path('api/', include(router.urls)),
 ]
