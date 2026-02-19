c9532b046ec9d11d646db9cf5928439c1427b732
Raúl Sánchez <rauulssanchezz@gmail.com>
2026-02-19 11:10:39 +0100

    espacio

--- rates/views.py
+++ rates/views.py
@@ -1,6 +1,5 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-
 from rates.models import Rate
 from rates.serializers import RatesSerializer
 from .permissions import IsOwnerOrReadOnly
