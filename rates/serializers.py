f0aae9546ae045de4c25ddaae404a34ea15805df
Raúl Sánchez <rauulssanchezz@gmail.com>
2026-02-19 10:56:48 +0100

    constantes

--- rates/serializers.py
+++ rates/serializers.py
@@ -2,18 +2,25 @@ from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 from .models import Rate
 
+ID = 'id'
+CLINIC = 'clinic'
+RATE = 'rate'
+USER = 'user'
+COMMENT = 'comment'
+USER_NAME = 'user_name'
+
 class RatesSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
     user_name = serializers.ReadOnlyField(source='user.username')
 
     class Meta:
         model = Rate
-        fields = ['id', 'clinic', 'rate', 'user', 'comment', 'user_name']
+        fields = [ID, CLINIC, RATE, USER, COMMENT, USER_NAME]
 
         validators = [
             UniqueTogetherValidator(
                 queryset=Rate.objects.all(),
-                fields=['user', 'clinic'],
+                fields=[USER, CLINIC],
                 message="Ya has dejado una valoración para esta clínica."
             )
         ]
